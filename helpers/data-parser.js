7e060e21e179358f66879a686bea42b3a60171cb
Stefan Krüger <stefan-krueger@hotmail.de>
2021-12-09 02:45:32 +0100

    solve day 5

--- helpers/data-parser.js
+++ helpers/data-parser.js
@@ -2,6 +2,7 @@ import {DAY1_DATA} from "../data/day1data.js";
 import {DAY2_DATA} from "../data/day2data.js";
 import {DAY3_DATA} from "../data/day3data.js";
 import {DAY4_DATA} from "../data/day4data.js";
+import {DAY5_DATA, DAY5_DATA_DEMO} from "../data/day5data.js";
 import {DAY6_DATA, DAY6_DATA_DEMO} from "../data/day6data.js";
 import {DAY7_DATA, DAY7_DATA_DEMO} from "../data/day7data.js";
 import {DAY8_DATA, DAY8_DATA_DEMO, DAY8_DATA_EXAMPLE} from "../data/day8data.js";
@@ -29,6 +30,10 @@ export function parseDay3Data() {
 
 export function parseDay4Data() {
     return DAY4_DATA.split('\n');
+}
+
+export function parseDay5Data() {
+    return DAY5_DATA.split('\n').map(aEntry => aEntry.split('->')).map(aPointValue => aPointValue.map(aVal => aVal.split(',').map(aVal => parseInt(aVal, 10))));
 }
 
 export function parseDay6Data() {
